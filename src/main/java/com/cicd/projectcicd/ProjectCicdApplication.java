c120372f6e370b107e780eeb49f4af34e90ec426
Vinay-AShield-Dev-io <vinay.kumara@ashield.io>
2022-11-08 18:59:16 +0530

    init project

---
+++ src/main/java/com/cicd/projectcicd/ProjectCicdApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.projectcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCicdApplication.class, args);
+	}
+
+}
